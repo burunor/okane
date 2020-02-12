b7e1d05f9c158095e3c02d1ebbf0d331049d2f11
Bruno Dantas <brunoroquedantas@gmail.com>
2020-02-12 20:09:58 -0300

    Add delete confirmation

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -96,22 +96,43 @@
                     <div class="row">
                         <div class="col-3 offset-9 d-flex">
                             <div class="pr-2">
-                                <form action="{{ route('product.destroy', ['id' => $product->id]) }}" method="post">
-                                    @method('DELETE')
-                                    @csrf
-                                    <button class="btn btn-danger">Delete</button>
-                                </form>
+                                <button class="btn btn-danger" type="button" data-toggle="collapse" data-target="#collapseDelete" aria-expanded="false" aria-controls="collapseDelete">Delete</button>
                             </div>
                             <div class="pr-2">
                                 <a href="{{ route('product.show', ['id' => $product->id]) }}" class="btn btn-outline-secondary">Cancel</a>
                             </div>
                             <div class="">
-                                <button class="btn btn-primary">Update</button>
+                                <button class="btn btn-primary" type="button">Update</button>
                             </div>
                         </div>
                     </div>
+
                 </div> <!-- End Col-->
             </div> <!-- End Row-->
         </form>
+        <div class="collapse" id="collapseDelete">
+            <div class="row align-content-center pt-4">
+                <div class="alert alert-danger col-12" role="alert">
+                    <h4 class="alert-heading">Alert!</h4>
+                    <p>You are deleting: {{ $product->name }}. This operation <strong>cannot</strong> be undone.</p>
+                    <p>Would you like to continue?</p>
+                    <hr>
+                    <div class="row">
+                        <div class="col-3 offset-9 d-flex">
+                            <div class="pr-2">
+                                <button class="btn btn-outline-secondary"  data-toggle="collapse" data-target="#collapseDelete" aria-expanded="false" aria-controls="collapseDelete">Cancel</button>
+                            </div>
+                            <div class="">
+                                <form action="{{ route('product.destroy', ['id' => $product->id])  }}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn btn-danger" type="button">Delete</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
